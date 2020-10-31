fd614f333f4298396edf03c79c533109f7dfda1a
DensetsuNoBaka <73606927+DensetsuNoBaka@users.noreply.github.com>
2020-10-30 23:30:55 -0400

    2020 10 29 (#1)
    * Fixed dependencies and SQL deployment script
    
    * Added dependency injected HeroHandler
    
    * Added dependency injection and started a framework to handle SQL sprocs
    
    Co-authored-by: Denis Bross <uchihaitachi111@gmail.com>

---
+++ Tour of Heroes API/Tour of Heroes/Tour of Heroes/Classes/HeroHandler.cs
@@ -0,0 +1,93 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Tour_of_Heroes.Entities;
+using Tour_of_Heroes.Interfaces;
+
+namespace Tour_of_Heroes.Classes
+{
+    public class HeroHandler : IHandler<Hero>
+    {
+        public HeroHandler()
+        {
+
+        }
+
+        public List<Hero> Get(int? heroId)
+        {
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["MyKey"].ConnectionString;
+
+            List<Hero> heroes = new List<Hero>();
+
+            string json = string.Empty;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("Hero_Get", conn);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Hero_ID", heroId);
+
+                    //open connection
+                    conn.Open();
+
+                    //execute the SQLCommand
+                    SqlDataReader dr = cmd.ExecuteReader();
+
+                    if (dr.HasRows)
+                    {
+                        while (dr.Read())
+                        {
+                            heroes.Add(new Hero
+                            {
+                                heroId = dr.GetInt32(0),
+                                heroName = dr.GetString(1),
+                                powerLevel = dr.GetString(2),
+                                pictureUrl = dr.GetString(3)
+                            });
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No data found.");
+                    }
+
+                    //close data reader
+                    dr.Close();
+
+                    //close connection
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                //display error message
+                Console.WriteLine("Exception: " + ex.Message);
+                throw ex;
+            }
+
+            return heroes;
+        }
+
+        public int Insert(Hero newRow)
+        {
+            return 0;
+        }
+
+        public void Update(Hero modifiedRow)
+        {
+
+        }
+        public void Delete(int id)
+        {
+
+        }
+    }
+}
